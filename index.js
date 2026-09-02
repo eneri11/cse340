ccc2a170381b1c09c07b52490c0e9e774955d18e
Irene Gavina Costales <>
2026-09-02 23:29:45 +0800

    Add HTTP server index.js file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-﻿console.log("Server is running!");
+﻿const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello from CSE340 on Render!");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
