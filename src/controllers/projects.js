788afd9b69360029823642a60f091df9b95ee140
Irene Gavina Costales <>
2026-09-23 21:24:04 +0800

    Export showProjectsPage in projects controller

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,5 +1,19 @@
-import { getProjectById, getCategoriesByProjectId } from '../models/projects.js';
+import { getAllProjects, getProjectById, getCategoriesByProjectId } from '../models/projects.js';
 
+// Controller to list projects (e.g., the upcoming five projects)
+export async function showProjectsPage(req, res, next) {
+    try {
+        const projects = await getAllProjects(); // or getUpcomingProjects() depending on your setup
+        res.render('projects', { 
+            title: 'Service Projects', 
+            projects 
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+// Controller for individual project details
 export async function showProjectDetailsPage(req, res, next) {
     try {
         const projectId = req.params.id;
