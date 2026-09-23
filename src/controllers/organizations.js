af741aee8bd6fbfeb2e897e7497ebceb5c0e2c2d
Irene Gavina Costales <>
2026-09-23 19:31:31 +0800

    Fix export name for showOrganizationDetailsPage

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,22 +1,23 @@
 import { getOrganizationById, getProjectsByOrganizationId } from '../models/organizations.js';
 
-export async function showOrganizationDetails(req, res, next) {
-  try {
-    const orgId = req.params.id;
-    const organization = await getOrganizationById(orgId);
-    
-    if (!organization) {
-      return res.status(404).render('404', { title: 'Organization Not Found' });
+// In src/controllers/organizations.js
+export async function showOrganizationDetailsPage(req, res, next) {
+    try {
+        const orgId = req.params.id;
+        const organization = await getOrganizationById(orgId);
+        
+        if (!organization) {
+            return res.status(404).render('404', { title: 'Organization Not Found' });
+        }
+
+        const projects = await getProjectsByOrganizationId(orgId);
+
+        res.render('organization-detail', {
+            title: organization.name,
+            organization,
+            projects
+        });
+    } catch (error) {
+        next(error);
     }
-
-    const projects = await getProjectsByOrganizationId(orgId);
-
-    res.render('organization-detail', {
-      title: organization.name,
-      organization,
-      projects
-    });
-  } catch (error) {
-    next(error);
-  }
 }
